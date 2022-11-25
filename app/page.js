1957930a4131252168b312735c3a8fa2ab5f9cb2
Erik Behrends <behrends@users.noreply.github.com>
2022-11-25 13:34:10 +0100

    Fix dhbwGrey Farbe

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="px-6">
       <main className="flex flex-col justify-center items-center min-h-screen py-6">
-        <h1 className="text-dhbwGray text-4xl">
+        <h1 className="text-dhbwGrey text-4xl">
           Block- und Studienpläne am SZI der DHBW Lörrach
         </h1>
 
@@ -10,15 +10,15 @@ export default function Home() {
 
         <div className="w-full">
           <div className="grid grid-cols-3 gap-4 flex-grow w-full text-dhbwRed">
-            <div className="h-20 card bg-zinc-200 rounded-box justify-center items-center shadow-xl">
+            <div className="h-20 card bg-zinc-200 hover:bg-zinc-400 rounded-box justify-center items-center shadow-xl cursor-pointer">
               <h2 className="card-title">TIF20A</h2>
             </div>
 
-            <div className="h-20 card bg-zinc-200 rounded-box justify-center items-center shadow-xl">
+            <div className="h-20 hover card bg-zinc-200 hover:bg-zinc-400 rounded-box justify-center items-center shadow-xl cursor-pointer">
               <h2 className="card-title">TIF22B</h2>
             </div>
 
-            <div className="h-20 card bg-zinc-200 rounded-box justify-center items-center shadow-xl">
+            <div className="h-20 card bg-zinc-200 hover:bg-zinc-400 rounded-box justify-center items-center shadow-xl cursor-pointer">
               <h2 className="card-title">WWI22B</h2>
             </div>
           </div>
@@ -27,7 +27,7 @@ export default function Home() {
 
       <footer className="flex justify-center items-center border-t border-slate-200 py-6">
         <a
-          className="text-dhbwGray"
+          className="text-dhbwGrey"
           href="https://www.dhbw-loerrach.de"
           target="_blank"
           rel="noopener noreferrer"

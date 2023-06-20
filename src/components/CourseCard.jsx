8eb782fb80d918281808f54a796c4b2ae8f08b41
Erik Behrends <behrends@users.noreply.github.com>
2023-06-20 15:23:48 +0200

    Add hover color

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,25 +1,31 @@
+import Link from 'next/link';
+
 export default function CourseCard({ course, color }) {
+  const hoverColor =
+    color === 'dhbwRed' ? 'hover:bg-red-100' : 'hover:bg-gray-100';
   return (
-    <div
-      className={`card p-2 mb-2 justify-center items-start  shadow-xl cursor-pointer border-2 border-${color} text-${color}`}
-    >
-      <div className="card-title">{course.name}</div>
-      <div className="text-sm mb-1">
-        {course.data?.start.toLocaleDateString('de')}-
-        {course.data?.end.toLocaleDateString('de')}
-      </div>
-      <div className="text-sm">
-        {course.data?.dates ? 'Termine:' : 'Keine Termine'}
-      </div>
-      {course.data?.dates?.map((d) => (
-        <div key={d.date} className="pl-4 text-sm">
-          {`${
-            typeof d.date === 'string'
-              ? d.date
-              : d.date.toLocaleDateString('de')
-          } ${d.description}`}
+    <Link href={`/courses/${course.name}`} key={course.name}>
+      <div
+        className={`card p-2 mb-2 justify-center items-start  shadow-xl cursor-pointer border-2 border-${color} text-${color} ${hoverColor}`}
+      >
+        <div className="card-title">{course.name}</div>
+        <div className="text-sm mb-1">
+          {course.data?.start.toLocaleDateString('de')}-
+          {course.data?.end.toLocaleDateString('de')}
         </div>
-      ))}
-    </div>
+        <div className="text-sm">
+          {course.data?.dates ? 'Termine:' : 'Keine Termine'}
+        </div>
+        {course.data?.dates?.map((d) => (
+          <div key={d.date} className="pl-4 text-sm">
+            {`${
+              typeof d.date === 'string'
+                ? d.date
+                : d.date.toLocaleDateString('de')
+            } ${d.description}`}
+          </div>
+        ))}
+      </div>
+    </Link>
   );
 }

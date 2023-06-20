26e972561f0e4eee876c496f057003b6a189518a
Erik Behrends <behrends@users.noreply.github.com>
2023-06-20 16:41:09 +0200

    Simplify course details page for now

--- src/app/courses/[course]/page.js
+++ src/app/courses/[course]/page.js
@@ -21,35 +21,16 @@ export default async function Course({ params }) {
   return (
     <>
       <h1 className="text-4xl">{course.name}</h1>
-      <p>Id: {course.id}</p>
-      <p>Semester: TODO</p>
       <p>
         <a
           className="text-dhbwRed"
           href={`https://webmail.dhbw-loerrach.de/owa/calendar/kal-${course.name.toLowerCase()}@dhbw-loerrach.de/Kalender/calendar.html`}
           target="_blank"
         >
-          Vorlesungskalender
+          Vorlesungskalender öffnen (externer Link)
         </a>
       </p>
-      <div className="text-red-900">TODO</div>
-      <h2 className="text-2xl">Termine</h2>
-      <ul>
-        <li>aktuell 2. Semester, Theorie an der DHBW Lörrach</li>
-        <li>
-          01.07.23 &mdash; Beginn der Praxisphase im Unternehmen (2.
-          Semester)
-          <ul className="pl-8">
-            <li>17.07.23 &mdash; Anmeldung 1. Projektarbeit</li>
-            <li>
-              17.07.23 &mdash; Mitteilung des gewählten Schwerpunkts
-            </li>
-          </ul>
-        </li>
-        <li>
-          01.10.23 &mdash; 3. Semesters, Theorie an der DHBW Lörrach
-        </li>
-      </ul>
+      <div className="text-red-900">TODO: weitere Infos anzeigen</div>
     </>
   );
 }

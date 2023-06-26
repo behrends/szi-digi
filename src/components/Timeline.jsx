2e05f384829294003ed0d6db10bbe2d5b4780b5e
Erik Behrends <behrends@users.noreply.github.com>
2023-06-26 16:56:35 +0200

    Properly set dates to midnight

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -28,44 +28,47 @@ export default function Timeline({ rows }) {
           let start, end;
           switch (e.target.value) {
             case 'Q223':
-              start = new Date('2023-03-31');
-              end = new Date('2023-07-01');
+              start = new Date('2023-04-01T00:00:00');
+              end = new Date('2023-06-30T00:00:00');
               break;
             case 'Q323':
-              start = new Date('2023-06-30');
-              end = new Date('2023-10-01');
+              start = new Date('2023-06-30T00:00:00');
+              end = new Date('2023-09-30T00:00:00');
               break;
             case 'Q423':
-              start = new Date('2023-09-30');
-              end = new Date('2024-01-01');
+              start = new Date('2023-10-01T00:00:00');
+              end = new Date('2023-12-31T00:00:00');
               break;
             case 'Q124':
-              start = new Date('2023-12-31');
-              end = new Date('2024-04-01');
+              start = new Date('2024-01-01T00:00:00');
+              end = new Date('2024-03-31T00:00:00');
               break;
             case 'Q224':
-              start = new Date('2024-03-31');
-              end = new Date('2024-07-01');
+              start = new Date('2024-04-01T00:00:00');
+              end = new Date('2024-06-30T00:00:00');
               break;
             case 'Q324':
-              start = new Date('2024-06-30');
-              end = new Date('2024-10-01');
+              start = new Date('2024-07-01T00:00:00');
+              end = new Date('2024-09-30T00:00:00');
               break;
             default:
               setCurrentData(completeData);
               return;
           }
-          const newData = completeData.filter((row) => {
-            if (
-              (row[4] > start && row[4] < end) ||
-              (row[5] > start && row[5] < end)
-            ) {
-              // TODO: Phasen am Rand des Quartals abschneiden?
-              // oder aktuelles Quartal hervorheben?
-              // --> Zeige heute an?
-              return row;
-            }
-          });
+          const newData = completeData
+            .filter((row) => {
+              return (
+                (row[4] >= start && row[4] <= end) ||
+                (row[5] >= start && row[5] <= end) ||
+                (row[4] <= start && row[5] >= end)
+              );
+            })
+            .map((row) => {
+              const newRow = [...row];
+              newRow[4] = newRow[4] <= start ? start : newRow[4];
+              newRow[5] = newRow[5] >= end ? end : newRow[5];
+              return newRow;
+            });
           setCurrentData(newData);
         }}
       >
@@ -93,10 +96,10 @@ export default function Timeline({ rows }) {
         chartType="Timeline"
         data={data}
         width="100%"
-        height="400px"
+        height="600px"
         options={{
           hAxis: {
-            format: 'dd.MM.yy',
+            format: 'dd.MM.yyyy',
           },
         }}
       />

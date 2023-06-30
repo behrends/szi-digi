41658d6dc388861e7e44813097aa31a629fee759
Erik Behrends <behrends@users.noreply.github.com>
2023-06-30 16:23:39 +0200

    Remove details from CourseCard

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,10 +1,6 @@
 import Link from 'next/link';
 
-export default function CourseCard({
-  course,
-  color,
-  showDates = false,
-}) {
+export default function CourseCard({ course, color }) {
   const hoverColor =
     color === 'dhbwRed' ? 'hover:bg-red-100' : 'hover:bg-gray-100';
   return (
@@ -13,26 +9,6 @@ export default function CourseCard({
         className={`card p-2 mb-2 mr-2 justify-center items-start shadow-xl cursor-pointer border-2 text-${color} ${hoverColor}`}
       >
         <div className="card-title text-xs">{course.name}</div>
-        {showDates && (
-          <div className="text-dhbwGrey">
-            <div className="text-sm font-medium mb-1">
-              {course.data?.start.toLocaleDateString('de')}-
-              {course.data?.end.toLocaleDateString('de')}
-            </div>
-            <div className="text-xs">
-              {course.data?.dates ? 'Termine:' : 'Keine Termine'}
-            </div>
-            {course.data?.dates?.map((d) => (
-              <div key={d.date} className="pl-4 text-xs">
-                {`${
-                  typeof d.date === 'string'
-                    ? d.date
-                    : d.date.toLocaleDateString('de')
-                } ${d.description}`}
-              </div>
-            ))}
-          </div>
-        )}
       </div>
     </Link>
   );
